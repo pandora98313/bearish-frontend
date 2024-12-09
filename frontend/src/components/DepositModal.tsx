ad783eee96d258b6e2177150d45fa1d675f37a7d
0xExp-po <xtinc.exp@gmail.com>
2024-12-09 10:29:01 +0900

    chore: update deposit modal

--- frontend/src/components/DepositModal.tsx
+++ frontend/src/components/DepositModal.tsx
@@ -2,8 +2,14 @@ import { useState } from "react";
 import Modal from "./ui/Modal"
 import Button from "./ui/Button";
 
-const DepositModal = ({ callback, setOpen }: Props) => {
+const DepositModal = ({
+  callback,
+  setOpen,
+  insufficientBalance = false
+}: Props) => {
   const [depositAmount, setDepositAmount] = useState<string>("0");
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [insufficientWalletBalance, _setInsufficientWalletBalance] = useState(true);
 
   const handleCancel = () => {
     setOpen(false);
@@ -19,23 +25,31 @@ const DepositModal = ({ callback, setOpen }: Props) => {
   return (
     <Modal title="Deposit" onClose={() => setOpen(false)}>
       <div className="flex flex-col gap-6">
+        {insufficientBalance && 
+          <p className="text-base leading-tight text-negative-light">You don’t have enough funds in your balance to place this bet. Deposit more or reduce your bet to continue.</p>
+        }
         <div className="flex flex-col gap-2">
-          <div className="flex justify-between items-center text-base text-gray-secondary">
+          <div className="flex justify-between items-center text-base leading-tight text-gray-secondary">
             <div><p>Deposit amount</p></div>
             <div className="flex gap-2">
               <p>$ 14.14</p>
               <p className="font-bold text-accent">Max</p>
             </div>
           </div>
-          <div className="flex justify-between items-center border border-gray-tertiary rounded-lg p-2 text-base">
-            <div className="flex items-center gap-2 text-gray-secondary">
-              <p>$</p>
-              <input type="text" value={depositAmount} onChange={(e) => setDepositAmount(e.target.value)} className="w-full" />
+          <div className="flex flex-col gap-1">
+            <div className={`flex justify-between items-center border ${insufficientWalletBalance ? 'border-negative-light' : 'border-gray-tertiary'} rounded-lg p-2 text-base leading-tight`}>
+              <div className="flex items-center gap-2 text-gray-secondary">
+                <p>$</p>
+                <input type="text" value={depositAmount} onChange={(e) => setDepositAmount(e.target.value)} className="w-full" />
+              </div>
+              <p className="text-gray-tertiary">USDC</p>
             </div>
-            <p className="text-gray-tertiary">USDC</p>
+            {insufficientWalletBalance &&
+              <p className="text-xs text-negative-light text-left">Not enough funds in your connected wallet.</p>
+            }
           </div>
         </div>
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center text-base leading-tight">
           <p>Balance</p>
           <div className="flex items-center gap-2 text-gray-secondary">
             <p>$ 0.15</p>
@@ -58,7 +72,8 @@ const DepositModal = ({ callback, setOpen }: Props) => {
 
 interface Props {
   callback: (result: boolean, depositedAmount?: number, transactionLink?: string) => void;
-  setOpen: (open: boolean) => void
+  setOpen: (open: boolean) => void;
+  insufficientBalance?: boolean;
 }
 
 export default DepositModal
