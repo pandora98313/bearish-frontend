712a14919bf2569950b2b4da0be3c7e53f2779a5
CreativeStar99 <159881729+pandora98313@users.noreply.github.com>
2025-04-03 14:06:15 +0900

    Update BetSection.tsx

--- frontend/src/components/BetSection.tsx
+++ frontend/src/components/BetSection.tsx
@@ -1,4 +1,3 @@
-import BetAmountSetSection from "./BetAmountSetSection"
 import BetButtonSection from "./BetButtonSection"
 import BettedUserSection from "./BettedUserSection"
 import AutoModeSection from "./AutoModeSection"
@@ -6,7 +5,6 @@ import AutoModeSection from "./AutoModeSection"
 const BetSection = () => {
   return (
     <div className="bg-card-primary rounded-lg p-4 flex flex-col gap-4">
-      <BetAmountSetSection />
       <BetButtonSection />
       <BettedUserSection />
       <AutoModeSection />
